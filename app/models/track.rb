cac794989cce3c6459639f72b99881a74f726a1f
therealmeyer <rmeyer@scu.edu>
2018-06-12 17:16:03 -0700

    Change audio back

--- app/models/track.rb
+++ app/models/track.rb
@@ -6,7 +6,7 @@ class Track < ApplicationRecord
   attr_reader :audio_remote_url, :image_remote_url
 
   has_attached_file :audio
-  validates_attachment_content_type :audio, content_type: "audio/*"
+  validates_attachment_content_type :audio, content_type: /\Aaudio\/.*\Z/
   #[ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
   #, content_type: /\Aaudio\/.*\Z/
   validates_attachment_presence :audio
