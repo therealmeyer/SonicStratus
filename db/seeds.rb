9426b24c9ab5d8c4efa1b04bd9bd5ec75ab3d4dd
therealmeyer <rmeyer@scu.edu>
2018-06-13 23:08:05 -0700

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,44 +17,58 @@ odesza = User.create(username: 'Odesza', password: 'inreturn',
   profile_image: File.open(Rails.root.join("app", "assets", "images/media_seeds/odesza.jpg")), 
   header_image: File.open(Rails.root.join("app", "assets", "images/media_seeds/moment_apart_tour.jpg")))
 
-drake = User.create(username: 'Drake', password: 'ovocrew')
+drake = User.create(username: 'Drake', password: 'ovocrew', 
+  profile_image: File.open(Rails.root.join("app", "assets", "images/media_seeds/ovo.jpg")), 
+  header_image: File.open(Rails.root.join("app", "assets", "images/media_seeds/drake-banner.jpg"))))
 
-foster = User.create(username: 'Foster', password: 'pumpedkicks')
+foster = User.create(username: 'Foster', password: 'pumpedkicks',
+  profile_image: File.open(Rails.root.join("app", "assets", "images/media_seeds/foster-user.jpg")), 
+  header_image: File.open(Rails.root.join("app", "assets", "images/media_seeds/foster-cover.jpg"))))
 
-flume = User.create(username: 'Flume', password: 'lockjaw')
+flume = User.create(username: 'Flume', password: 'lockjaw', 
+  profile_image: File.open(Rails.root.join("app", "assets", "images/media_seeds/flume-user.jpg")), 
+  header_image: File.open(Rails.root.join("app", "assets", "images/media_seeds/flume-cover.jpg"))))
 
-emmit = User.create(username: 'Emmit Fenn', password: 'password')
+emmit = User.create(username: 'Emmit Fenn', password: 'password', 
+  profile_image: File.open(Rails.root.join("app", "assets", "images/media_seeds/emmit-fenn-user.jpg")), 
+  header_image: File.open(Rails.root.join("app", "assets", "images/media_seeds/emitt-fenn-cover.jpg"))))
 
 
 whitelies = Track.create(user_id: odesza.id, title: "White Lies", 
-  description: "From the album 'In Return', out now on Counter-Records.
-  Download 'In Return' on iTunes. Order 'In Return' in 2xLP Vinyl, 24-bit WAV, 16-bit WAV, and MP3.", 
+  description: "From the album 'In Return', out now on Counter-Records.\n
+  Download 'In Return' on iTunes. \nOrder 'In Return' in 2xLP Vinyl, 24-bit WAV, 16-bit WAV, and MP3.", 
   year: 2015,
   image: File.open(Rails.root.join("app", "assets", "images/media_seeds/in-return.jpg")), 
   audio: File.open(Rails.root.join("app", "assets", "images/media_seeds/white-lies.mp3")))
 
 how = Track.create(user_id: odesza.id, title: "How Did I Get Here", year: 2013,
-  description: "Stream and Download Summer's Gone Download our official app",
+  description: "Stream and Download Summer's Gone \n \nDownload our official app",
   image: File.open(Rails.root.join("app", "assets", "images/media_seeds/summers-gone.jpg")), 
   audio: File.open(Rails.root.join("app", "assets", "images/media_seeds/how-did-i.mp3")))
 
-meridian = Track.create(user_id: odesza.id, title: "Meridian", 
+meridian = Track.create(user_id: odesza.id, title: "Meridian", year: 2017,
+  description: " Imaginary circle in a plane perpendicular to the planes of the celestial equator and horizon\n\n
+  An imaginary arc on the Earth's surface from the North Pole to the South Pole",
   image: File.open(Rails.root.join("app", "assets", "images/media_seeds/moment-apart.jpg")), 
   audio: File.open(Rails.root.join("app", "assets", "images/media_seeds/meridian.mp3")))
 
-boy = Track.create(user_id: odesza.id, title: "Boy", 
+boy = Track.create(user_id: odesza.id, title: "Boy", year: 2017,
+  description: "A boy is a young male human, usually a child or adolescent. \n When he becomes an adult, he is described as a man.",
   image: File.open(Rails.root.join("app", "assets", "images/media_seeds/moment-apart.jpg")), 
   audio: File.open(Rails.root.join("app", "assets", "images/media_seeds/boy.mp3")))
 
-gp = Track.create(user_id: drake.id, title: "God's Plan", 
+gp = Track.create(user_id: drake.id, title: "God's Plan", year: 2018,
+  description: "God's plan, or the Will of God. \n The saving of a soul from sin and its consequences",
   image: File.open(Rails.root.join("app", "assets", "images/media_seeds/scary_hours.jpg")), 
   audio: File.open(Rails.root.join("app", "assets", "images/media_seeds/gods-plan.mp3")))
 
-sit = Track.create(user_id: foster.id, title: "Sit Next to Me", 
+sit = Track.create(user_id: foster.id, title: "Sit Next to Me", year: 2018,
+  description: "Released by:\nColumbia\n\nRelease date:\n21 July 2017\n\nP-line:\n
+℗ 2017 (P) 2017 Columbia Records, a Division of Sony Music Entertainment",
   image: File.open(Rails.root.join("app", "assets", "images/media_seeds/sacred-hearts.jpg")), 
   audio: File.open(Rails.root.join("app", "assets", "images/media_seeds/sit-next-to-me.mp3")))
 
-pumpedkicks = Track.create(user_id: foster.id, title: "Pumped Up Kicks", 
+pumpedkicks = Track.create(user_id: foster.id, title: "Pumped Up Kicks", year: 2010,
   image: File.open(Rails.root.join("app", "assets", "images/media_seeds/torches.jpg")), 
   audio: File.open(Rails.root.join("app", "assets", "images/media_seeds/pumpedup-kicks.mp3")))
   
