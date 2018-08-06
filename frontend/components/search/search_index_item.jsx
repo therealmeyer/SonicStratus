ed7d3f52c9e9e0f66079fd2c389def68a441a6a7
therealmeyer <rmeyer@scu.edu>
2018-08-06 15:39:51 -0700

    remove console logs

--- frontend/components/search/search_index_item.jsx
+++ frontend/components/search/search_index_item.jsx
@@ -19,14 +19,13 @@ class SearchIndexItem extends React.Component {
   }
 
   render() {
-    console.log(this.props.i, this.state.background);
     const {result} = this.props;
     if (result.searchable_type === "User") {
       return (
         <Link to={`/users/${result.searchable_id}`}>
           <li className="search-item" 
-            onMouseEnter={() => {this.setState({ background: "#333" }, () => this.setState({background: "#333"})); console.log("enter")}}
-            onMouseLeave={() => {this.setState({ background: "black" }); console.log("leave")}} 
+            onMouseEnter={() => {this.setState({ background: "#333" }, () => this.setState({background: "#333"}))}}
+            onMouseLeave={() => this.setState({ background: "black" })} 
             style={{ paddingLeft: "15px", background: this.state.background }} 
             key={result.searchable_id}>
             <i className="fa fa-search search-item-icon" aria-hidden="true"></i>
@@ -38,8 +37,8 @@ class SearchIndexItem extends React.Component {
       return (
         <Link to={`/tracks/${result.searchable_id}`}>
           <li className="search-item" 
-            onMouseEnter={() => {this.setState({ background: "#333" }); console.log("enter");}}
-            onMouseLeave={() => {this.setState({ background: "black" }); console.log("leave");}} 
+            onMouseEnter={() => this.setState({ background: "#333" })}
+            onMouseLeave={() => this.setState({ background: "black" })} 
             style={{ paddingLeft: "15px", background: this.state.background }} 
             key={result.searchable_id}>
             <i className="fa fa-search search-item-icon" aria-hidden="true"></i>
